5f777e7b5a31cd5f04fe548136aa772f592d0cb6
HardenTheBest <1525468109@qq.com>
2023-12-13 21:52:48 +0800

    添加登录拦截器，如果用户没有登录就获取数据则报错

---
+++ src/main/java/com/zjut/abms/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package com.zjut.abms.config;
+
+import com.zjut.abms.interceptors.LoginProtectedInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private LoginProtectedInterceptor loginProtectedInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginProtectedInterceptor).addPathPatterns("/flight/**");
+    }
+}
---
+++ src/main/java/com/zjut/abms/interceptors/LoginProtectedInterceptor.java
@@ -0,0 +1,43 @@
+package com.zjut.abms.interceptors;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zjut.abms.utils.JwtHelper;
+import com.zjut.abms.utils.Result;
+import com.zjut.abms.utils.ResultCodeEnum;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+/**
+ * 登录包含拦截器，检查请求头是否包含有效token
+ * 有，放行
+ * 没有，返回504
+ */
+@Component
+public class LoginProtectedInterceptor implements HandlerInterceptor {
+    @Autowired
+    private JwtHelper jwtHelper;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //从请求头中获取token
+        String token = request.getHeader("token");
+        //检查是否有效
+        boolean expiration = jwtHelper.isExpiration(token);
+        //有效放行
+        if (!expiration) {
+            return true;
+        }
+
+        //无效返回504的状态json
+        Result<Object> result = Result.build(null, ResultCodeEnum.NOTLOGIN);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(result);
+        response.getWriter().write(json);
+
+        return false;
+    }
+}
